4bddf332fb0877eafd3e3a2efe5230bd24157688
Naufal Muafi <nmuafi1@gmail.com>
2023-10-20 21:28:38 +0700

    add: payment type function

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -30,7 +30,7 @@ def c_daily_orders(df):
   })
   daily_orders = daily_orders.reset_index()
   daily_orders.rename(columns={
-      "order_purchase_timestamp": "order_date",
+    "order_purchase_timestamp": "order_date",
     "order_id": "order_count",
     "price": "revenue"
   }, inplace=True)
@@ -41,7 +41,7 @@ def c_daily_orders(df):
 def c_sum_order(df):
   sum_order = df.groupby("product_category_name").order_id.nunique().sort_values(ascending=False).reset_index()
   sum_order.rename(columns={
-      "order_id": "quantity"
+    "order_id": "quantity"
   }, inplace=True)
   
   return sum_order
@@ -49,8 +49,8 @@ def c_sum_order(df):
 # ===== by City Dataframe =====
 def c_bycity(df):
   bycity = df.groupby(by="customer_city").customer_id.nunique().reset_index()
-  bycity.reanme(columns={
-      "customer_id": "customer_count"
+  bycity.rename(columns={
+    "customer_id": "customer_count"
   }, inplace=True)
   
   return bycity
@@ -58,8 +58,19 @@ def c_bycity(df):
 # ===== by State Dataframe =====
 def c_bystate(df):
   bystate = df.groupby(by="customer_state").customer_id.nunique().reset_index()
-  bystate.reanme(columns={
-      "customer_id": "customer_count"
+  bystate.rename(columns={
+    "customer_id": "customer_count"
   }, inplace=True)
   
   return bystate
+
+# ===== Payment Type Dataframe =====
+def c_payment_type(df):
+  payment_type = df.groupby(by="payment_type").order_id.nunique().reset_index()
+  payment_type.rename(columns={
+    "order_id": "order_count"
+  }, inplace=True)
+  
+  return payment_type
+
+# ===== RFM Dataframe =====
