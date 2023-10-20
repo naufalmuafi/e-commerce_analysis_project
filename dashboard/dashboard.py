df8414dcc75ca542c61384bf69ac84a58990d360
Naufal Muafi <nmuafi1@gmail.com>
2023-10-20 21:03:40 +0700

    add: initial dashboard

---
+++ dashboard/dashboard.py
@@ -0,0 +1,25 @@
+''' ====================
+
+E-Commerce Public Analysis Dashboard
+
+by Naufal Mu'afi
+nmuafi1@gmail.com
+
+==================== '''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+
+sns.set(style="dark")
+
+
+''' ====================
+
+Dataframe Preparation
+
+==================== '''
+
+# ===== Daily Orders Dataframe =====
