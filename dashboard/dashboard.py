09720506b9a57d8f6676425cdd7c3aaecb9277a8
Naufal Muafi <nmuafi1@gmail.com>
2023-10-20 21:20:54 +0700

    add: sum orders function

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -30,8 +30,19 @@ def c_daily_orders(df):
   })
   daily_orders = daily_orders.reset_index()
   daily_orders.rename(columns={
+      "order_purchase_timestamp": "order_date",
     "order_id": "order_count",
     "price": "revenue"
   }, inplace=True)
   
   return daily_orders
+
+# ===== Sum Order Dataframe =====
+def c_sum_order(df):
+  sum_order = df.groupby("product_category_name").order_id.nunique().sort_values(ascending=False).reset_index()
+  sum_order.rename(columns={
+      "order_id": "quantity"
+  }, inplace=True)
+  
+  return sum_order
+
